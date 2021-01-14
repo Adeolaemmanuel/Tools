0bd1ceb17b9a246395382f063439789c47bfec08
Adeolaemmanuel <adeolaemmanuel006@gmail.com>
2021-01-14 10:01:52 +0100

    first commit

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.post('/sign', (req, res)=>{
             software to give our customers the best. Our referal program also help 
             our customers earn more.
           </h4>
-
+          <b>Verification code: ${req.body.verify}</b>
     `
   });
 })
