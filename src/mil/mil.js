106e387e0dbbd88d6ec86958ea0c6483b3747ea2
Adeolaemmanuel <adeolaemmanuel006@gmail.com>
2021-01-20 11:39:41 +0100

    modified

--- src/mil/mil.js
+++ src/mil/mil.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import logo from '../assets/img/mill/logo.png'
 import homeBg from '../assets/img/mill/homeBg.jpg'
+import p from '../assets/img/mill/p.jpeg'
 import { db, storage } from '../database'
 
 export default class Mil extends Component {
@@ -26,7 +27,21 @@ export default class Mil extends Component {
             dns: '',
             mmn: '',
             nok: '',
-            profilePic: '',
+            m1: '',
+            m2: '',
+            m3: '',
+            m4: '',
+            m5: '',
+            m6: '',
+            m7: '',
+            at: '',
+            as: '',
+            an2: '',
+            an: '',
+            ab: '',
+            bn: '',
+            rn: '',
+            pt: '',
         }
     }
 
@@ -37,18 +52,6 @@ export default class Mil extends Component {
                 bgHeight: '700px'
             })
         }
-
-        storage.ref('picture').getDownloadURL().then((p) => {
-            fetch(p,
-                {
-                    mode: 'cors',
-                    cache: 'no-cache',
-                    credentials: 'same-origin',
-                }
-            ).then(r => { console.log(r.json) })
-            this.setState({ profilePic: p })
-            console.log(this.state.profilePic)
-        })
 
         db.collection('cuzo').doc('Details').get()
             .then(d => {
@@ -70,9 +73,17 @@ export default class Mil extends Component {
                     as: d.data().as,
                     an2: d.data().an2,
                     an: d.data().an,
+                    ab: d.data().ab,
                     bn: d.data().bn,
                     rn: d.data().rn,
                     pt: d.data().pt,
+                    m1: d.data().m1,
+                    m2: d.data().m2,
+                    m3: d.data().m3,
+                    m4: d.data().m4,
+                    m5: d.data().m5,
+                    m6: d.data().m6,
+                    m7: d.data().m7,
                 })
             })
     }
@@ -160,14 +171,12 @@ export default class Mil extends Component {
                     <div className='w3-row'>
                         <div className='w3-col m3 l3 w3-hide-small'><br /></div>
                         <div className='w3-col m12 m7 l7'>
-                            <div className="w3-container w3-text-green w3-border">SECURED US MARINE MEMBERS PROFILE</div>
+                            
                             <div className='w3-row'>
                                 <div className='w3-col s3 m3 l3 w3-padding'>
 
                                 </div>
-                                <div className='w3-col s3 m3 l3 w3-padding'>
-                                    <img src={this.state.profilePic} alt='' />
-                                </div>
+                                
                                 <div className='w3-col s3 m3 l3 w3-padding'>
 
                                 </div>
@@ -176,6 +185,12 @@ export default class Mil extends Component {
                     </div>
                     <div className='w3-row'>
                         <div className='w3-col s12 m4 l4 w3-padding'>
+                            <div className='w3-container w3-text-green w3-border w3-center'>
+                                <span className='w3-padding w3-tetxt-green'>SECURED US MARINE MEMBERS PROFILE</span>
+                            </div>
+                            <div className='w3-container w3-center'>
+                                <img src={p} alt='' style={{ width: '100%', height: '200px' }} />
+                            </div>
                             <div className='w3-container w3-text-green w3-border w3-center'>
                                 <span className='w3-padding w3-tetxt-green'>BASIC DATA</span>
                             </div>
@@ -198,29 +213,40 @@ export default class Mil extends Component {
                             <div className='w3-container w3-text-green w3-border w3-center'>
                                 <span className='w3-padding w3-tetxt-green'>SALARY/BENEFITS PROCESSING DATA</span>
                             </div>
-                            <div className="w3-container w3-border w3-small" style={{backgroundColor: '#f0f0ea'}}>
-                                <p>Payment type: Direct Wire Transfer</p>
-                                <p>Bank Name: Bank of America</p>
-                                <p>Account Name: Micheal Brandon</p>
-                                <p>Account Number: ***********46738</p>
-                                <p>Routing Number: 0093020</p>
-                                <p>Account type: Checking</p>
-                                <p>Account Status: Temporary Frozen</p>
+                            <div className="w3-container w3-border w3-small" style={{ backgroundColor: '#f0f0ea' }}>
+                                <p>Payment type: {this.state.pt}</p>
+                                <p>Bank Name: {this.state.bn}</p>
+                                <p>Account Name: {this.state.an}</p>
+                                <p>Account Number: ***********{this.state.an2}</p>
+                                <p>Routing Number: {this.state.rn}</p>
+                                <p>Account type: {this.state.at}</p>
+                                <p>Account Status: {this.state.as}</p>
                                 <p id="account" className="w3-btn w3-round w3-white">Check Account Status</p>
                             </div>
                         </div>
-                        <div className='w3-col m12 m4 l4'>
+                        <div className='w3-col m12 m4 l4 w3-margin-top'>
                             <div className='w3-container w3-text-green w3-border w3-center'>
                                 <span className='w3-padding w3-tetxt-green'>DEPLOYMENT HISTORY TILL DATE</span>
                             </div>
-                            <ul className="w3-container w3-border w3-small" style={{backgroundColor: '#f0f0ea'}}>
-                                <li>Invasion of Afghanistan (2009)</li>
-                                <li>Iraq war ( 2013 )</li>
-                                <li>Bridge Construction Halti Rebelion (2014) </li>
-                                <li>Adana (2014-2015</li>
-                                <li>Peace keeping- Turkey  ( 2019 - Present )</li>
-
-                            </ul>
+                            <div className="w3-container w3-border w3-small" style={{ backgroundColor: '#f0f0ea' }}>
+                                <p>{this.state.m1}</p>
+                                <p>{this.state.m2}</p>
+                                <p>{this.state.m3}</p>
+                                <p>{this.state.m4}</p>
+                                <p>{this.state.m5}</p>
+                                <p>{this.state.m6}</p>
+                                <p>{this.state.m7}</p>
+                            </div>
+                        </div>
+                        <div className='w3-col m12 m4 l4 w3-margin-top'>
+                            <div className='w3-container w3-text-green w3-border w3-center'>
+                                <span className='w3-padding w3-tetxt-green'>MY MENU</span>
+                            </div>
+                            <div className="w3-container w3-border w3-small w3-margin-bottom" style={{ backgroundColor: '#f0f0ea' }}>
+                                <button className='w3-btn w3-grey w3-padding w3-block w3-margin-top w3-round w3-padding w3-text-white' onClick={() => alert(this.state.m7)}>Current Mission</button>
+                                <button className='w3-btn w3-grey w3-padding w3-block w3-margin-top w3-round w3-padding w3-text-white' onClick={() => alert(this.state.ab)}>Current Balance</button>
+                                <button className='w3-btn w3-grey w3-padding w3-block w3-margin-top w3-round w3-padding w3-text-white' onClick={() => window.location.reload()}>Logout</button>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -342,14 +368,22 @@ class Admin extends Component {
             dns: e.target.elements.dns.value,
             mmn: e.target.elements.mmn.value,
             nok: e.target.elements.nok.value,
-            pass: e.target.elements.pass,
+            pass: e.target.elements.pass.value,
             at: e.target.elements.at.value,
             as: e.target.elements.as.value,
             an2: e.target.elements.an2.value,
             an: e.target.elements.an.value,
+            ab: e.target.elements.ab.value,
             bn: e.target.elements.bn.value,
             rn: e.target.elements.rn.value,
             pt: e.target.elements.pt.value,
+            m1: e.target.elements.m1.value,
+            m2: e.target.elements.m2.value,
+            m3: e.target.elements.m3.value,
+            m4: e.target.elements.m4.value,
+            m5: e.target.elements.m5.value,
+            m6: e.target.elements.m6.value,
+            m7: e.target.elements.m7.value,
         }
 
         
@@ -396,12 +430,17 @@ class Admin extends Component {
             return(
                 <div>
                     <div className='w3-padding'>
-                        <div>
+                        {
+                            /*
+                             <div>
                             <label htmlFor='profile' className='w3-btn w3-green w3-round'>Upload profile picture</label>
                             <input type='file' className='w3-hide' id='profile' onChange={e => { this.upload(e, 'profile') }} />
                             <label htmlFor='logo' className='w3-btn w3-green w3-round'>Upload military logo</label>
                             <input type='file' className='w3-hide' id='logo' onChange={e => { this.upload(e, 'profile') }}  />
                         </div>
+                             
+                             */
+                        }
                         <form onSubmit={e => { this.send(e) }} >
                             <div className='w3-center'>
                                 <span className='w3-padding w3-tetxt-green'>BASIC DATA</span>
@@ -429,6 +468,7 @@ class Admin extends Component {
                             </div>
                             <input className='w3-input w3-border w3-round w3-margin-top' type='text' id='pt' placeholder='Payment type' />
                             <input className='w3-input w3-border w3-round w3-margin-top' type='text' id='bn' placeholder='Bank Name' />
+                            <input className='w3-input w3-border w3-round w3-margin-top' type='text' id='ab' placeholder='Acc balance' />
                             <input className='w3-input w3-border w3-round w3-margin-top' type='text' id='an' placeholder='Acc Name' />
                             <input className='w3-input w3-border w3-round w3-margin-top' type='text' id='an2' placeholder='Acc Num' />
                             <input className='w3-input w3-border w3-round w3-margin-top' type='text' id='rn' placeholder='Routing number' />
@@ -436,8 +476,16 @@ class Admin extends Component {
                             <input className='w3-input w3-border w3-round w3-margin-top' type='text' id='at' placeholder='Acc typ' />
 
                             <div className='w3-center'>
-                                <span className='w3-padding w3-tetxt-green'>SALARY/BENEFITS</span>
+                                <span className='w3-padding w3-tetxt-green'>MISSIONS</span>
                             </div>
+
+                            <input className='w3-input w3-border w3-round w3-margin-top' type='text' id='m1' placeholder='missio 1' />
+                            <input className='w3-input w3-border w3-round w3-margin-top' type='text' id='m2' placeholder='missio 2' />
+                            <input className='w3-input w3-border w3-round w3-margin-top' type='text' id='m3' placeholder='missio 3' />
+                            <input className='w3-input w3-border w3-round w3-margin-top' type='text' id='m4' placeholder='missio 4' />
+                            <input className='w3-input w3-border w3-round w3-margin-top' type='text' id='m5' placeholder='missio 5' />
+                            <input className='w3-input w3-border w3-round w3-margin-top' type='text' id='m6' placeholder='missio 6' />
+                            <input className='w3-input w3-border w3-round w3-margin-top' type='text' id='m7' placeholder='missio 7' />
 
                             <button className='w3-btn w3-block w3-margin-top w3-round w3-green'>Update Idiot</button>
                         </form>
